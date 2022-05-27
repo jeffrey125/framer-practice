86e0b05a173a40950afdb8817d37aebd802158f4
jeffrey125 <jeffreyasilo8@gmail.com>
2022-05-27 18:06:09 +0800

    Added Framer-motion animation

---
+++ src/components/Modal.js
@@ -0,0 +1,50 @@
+import { Link } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
+import React from 'react';
+
+const backdropVariants = {
+  visible: { opacity: 1 },
+  hidden: { opacity: 0 },
+};
+
+const modalVariants = {
+  hidden: { y: '-100vh', opacity: 0 },
+  visible: {
+    y: '200px',
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+    },
+  },
+};
+
+const Modal = ({ showModal, setShowModal }) => {
+  return (
+    <AnimatePresence exitBeforeEnter>
+      {showModal && (
+        <motion.div
+          className="backdrop"
+          variants={backdropVariants}
+          animate="visible"
+          initial="hidden"
+          exit="hidden"
+        >
+          <motion.div className="modal" variants={modalVariants} exit="hidden">
+            <p>Want to make another pizza?</p>
+            <Link to="/">
+              <button
+                onClick={() => {
+                  setShowModal(false);
+                }}
+              >
+                Start again!
+              </button>
+            </Link>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Modal;
